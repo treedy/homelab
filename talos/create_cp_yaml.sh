9fb1b321cb02f0c09460bb4b19cfbe2824afbc7c
Todd Reedy <1851066+treedy@users.noreply.github.com>
2025-04-03 09:44:34 -0400

    update(talos): Modify gateway IP and add nameservers in patch YAML files

--- talos/create_cp_yaml.sh
+++ talos/create_cp_yaml.sh
@@ -1,9 +1,9 @@
 talosctl gen config homek8s https://192.168.0.1:6443 \
 	-t controlplane \
 	--config-patch @certsans.patch.yaml \
-	--config-patch @cp02.patch.yaml \
+	--config-patch @cp${1}.patch.yaml \
 	--config-patch @hpED800G3_cp_image.patch.yaml \
 	--with-secrets cluster_secrets.yaml \
 	--with-docs=false \
 	--with-examples=false \
-	-o cp02.yaml 
+	-o cp${1}.yaml
